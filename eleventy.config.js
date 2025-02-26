23f2b9a4c8530956b96a9bd50386793256b97caa
Luke Hansford <mail@lukehansford.me>
2025-02-26 14:24:19 +0800

    Add support for links

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,20 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import MarkdownIt from "markdown-it";
 
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles/*.css");
   eleventyConfig.addPassthroughCopy("src/images/*.svg");
 
   eleventyConfig.addPlugin(pluginWebc, { components: "src/_components/**/*.webc" });
+
+  const md = new MarkdownIt({ linkify: true });
+
+  eleventyConfig.addJavaScriptFunction("fromMarkdown", function(markdown) {
+    return md.render(markdown);
+  });
+
+  eleventyConfig.addJavaScriptFunction("fromMarkdownInline", function(markdown) {
+    return md.renderInline(markdown);
+  });
 };
 
